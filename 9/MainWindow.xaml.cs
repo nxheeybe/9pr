9c41d6f858324d637bbd974a3a4fdf9c808d6e4d
nxheeybe <144349725+nxheeybe@users.noreply.github.com>
2024-09-23 01:56:19 +0300

    Add files via upload

---
+++ 9/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection.Metadata;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Spire.Doc;
+namespace _9
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void createWordFile_Click(object sender, RoutedEventArgs e)
+        {
+            WordWindow wordWindow = new WordWindow("ааа");
+            wordWindow.Show();
+        }
+
+        private void openWordFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenWordFile();
+        }
+
+        private void OpenWordFile()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.Filters.Add(new CommonFileDialogFilter("Word файл", "*.docx;*.doc"));
+            dialog.Title = "Выберите Word-файл";
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                string path = dialog.FileName;
+                Document doc = new Document();
+                doc.LoadFromFile(dialog.FileName);
+                doc.SaveToFile(dialog.FileName, FileFormat.Rtf);
+
+                WordWindow wordWindow = new WordWindow(path);
+                wordWindow.LoadRtfFile(dialog.FileName);
+                wordWindow.Show();
+            }
+        }
+
+        private void createExcelFile_Click(object sender, RoutedEventArgs e)
+        {
+            ExcelWindow excelWindow = new ExcelWindow();
+            excelWindow.Show();
+        }
+
+        private void openExcelFile_Click(object sender, RoutedEventArgs e)
+        {
+            ExcelWindow excelWindow = new ExcelWindow();
+            excelWindow.Show();
+        }
+    }
+}
---
+++ 9/WordWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace _9
+{
+    namespace _9
+    {
+        public partial class WordWindow : Window
+        {
+            public string path;
+
+            public WordWindow(string path)
+            {
+                InitializeComponent();
+                this.path = path;
+            }
+
+            private void sendEmail_Click(object sender, RoutedEventArgs e)
+            {
+                if (SaveFile())
+                {
+                    SendEmail sendEmail = new SendEmail(path);
+                    sendEmail.Show();
+                }
+            }
+
+            private void saveFile_Click(object sender, RoutedEventArgs e)
+            {
+                SaveFile();
+            }
+
+            private bool SaveFile()
+            {
+                CommonSaveFileDialog dialog = new CommonSaveFileDialog();
+                dialog.Filters.Add(new CommonFileDialogFilter("Word файл", "*.docx"));
+
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    string fileName = dialog.FileName;
+
+                    if (!fileName.EndsWith(".docx"))
+                    {
+                        fileName += ".docx";
+                    }
+
+                    var range = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
+                    using (var fs = new FileStream(fileName, FileMode.Create))
+                    {
+                        range.Save(fs, DataFormats.Rtf);
+                    }
+
+                    Document document = new Document();
+                    document.LoadFromFile(fileName);
+                    document.SaveToFile(fileName, FileFormat.Docx);
+
+                    path = fileName;
+
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void LoadRtfFile(string filePath)
+            {
+                var range = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
+                using (var fs = new FileStream(filePath, FileMode.OpenOrCreate))
+                {
+                    range.Load(fs, DataFormats.Rtf);
+                }
+            }
+        }
+    }
+}
